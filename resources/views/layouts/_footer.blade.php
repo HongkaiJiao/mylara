618017c6c685be0299e45e486081a4928006e49d
HongkaiJiao <basketballjhk@163.com>
2020-04-12 11:51:24 +0000

    [update]基础基础页面构建.

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+            由 <a href="https://github.com/HongkaiJiao/" target="_blank">KevinJiao</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="float-right"><a href="mailto:273912572@qq.com">联系作者</a></p>
+    </div>
+</footer>
